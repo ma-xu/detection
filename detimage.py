1fc5e4e802fd5250e343026103103104e860c3c3
13952522076 <1007540910@qq.com>
2019-11-22 21:27:21 -0600

    1111

--- detimage.py
+++ detimage.py
@@ -3,12 +3,12 @@ from mmcv.runner import load_checkpoint
 from mmdet.models import build_detector
 from mmdet.apis import inference_detector, show_result
 
-cfg = mmcv.Config.fromfile('local_configs/cascade_rcnn_r50_fpn_20e_pretrain_gc_resnet50.py')
+cfg = mmcv.Config.fromfile('local_configs/cascade_rcnn_r50_fpn_20e_pretrain_res1_gc_resnet50.py')
 cfg.model.pretrained = None
 
 # construct the model and load checkpoint
 model = build_detector(cfg.model, test_cfg=cfg.test_cfg)
-_ = load_checkpoint(model, './work_dirs/cascade_rcnn_r50_fpn_20e_pretrain_gc_resnet50/epoch_24.pth')
+_ = load_checkpoint(model, './work_dirs/cascade_rcnn_r50_fpn_20e_pretrain_res1_gc_resnet50/epoch_24.pth')
 
 # test a single image
 # img = mmcv.imread('test.jpg')
