655f8f95deea0456a5b5f2274ea796b03354f6eb
13952522076 <xuma@my.unt.edu>
2020-01-19 22:17:43 -0600

    1

--- local_configs/retinanet_r50_fpn_2x_pretrain_prm_resnet50.py
+++ local_configs/retinanet_r50_fpn_2x_pretrain_prm_resnet50.py
@@ -56,7 +56,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
     imgs_per_gpu=2,
-    workers_per_gpu=2,
+    workers_per_gpu=1,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
