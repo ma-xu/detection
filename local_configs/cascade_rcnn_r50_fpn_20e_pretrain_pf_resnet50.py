c7413a97b46af0a54d4c90147f02bc728f485623
13952522076 <xuma@my.unt.edu>
2020-01-30 01:51:52 -0600

    1

--- local_configs/cascade_rcnn_r50_fpn_20e_pretrain_pf_resnet50.py
+++ local_configs/cascade_rcnn_r50_fpn_20e_pretrain_pf_resnet50.py
@@ -207,5 +207,5 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_r50_fpn_20e_pretrain_pf_resnet50'
 load_from = None
-resume_from = None
+resume_from = './work_dirs/cascade_rcnn_r50_fpn_20e_pretrain_pf_resnet50/epoch_1.pth'
 workflow = [('train', 1)]
