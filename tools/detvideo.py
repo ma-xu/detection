af874220b1de73fdd30d8bb5117247fec34a18d6
13952522076 <xuma@my.unt.edu>
2020-02-06 13:37:08 -0600

    1

--- tools/detvideo.py
+++ tools/detvideo.py
@@ -3,12 +3,12 @@ from mmcv.runner import load_checkpoint
 from mmdet.models import build_detector
 from mmdet.apis import inference_detector, show_result
 
-cfg = mmcv.Config.fromfile('local_configs/retinanet_r50_fpn_2x_pretrain_na_resnet50.py')
+cfg = mmcv.Config.fromfile('../local_configs/retinanet_r50_fpn_2x_pretrain_na_resnet50.py')
 cfg.model.pretrained = None
 
 # construct the model and load checkpoint
 model = build_detector(cfg.model, test_cfg=cfg.test_cfg)
-_ = load_checkpoint(model, './work_dirs/retinanet_r50_fpn_2x_pretrain_na_resnet50/epoch_24.pth')
+_ = load_checkpoint(model, '../work_dirs/retinanet_r50_fpn_2x_pretrain_na_resnet50/epoch_24.pth')
 
 """
 # test a single image
