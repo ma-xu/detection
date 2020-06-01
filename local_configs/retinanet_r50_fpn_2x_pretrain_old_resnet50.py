53232b16e3438eee57ce12e52be64e852c284999
13952522076 <xuma@my.unt.edu>
2020-06-01 11:07:44 -0500

    update

--- local_configs/retinanet_r50_fpn_2x_pretrain_old_resnet50.py
+++ local_configs/retinanet_r50_fpn_2x_pretrain_old_resnet50.py
@@ -2,7 +2,7 @@
 model = dict(
     type='RetinaNet',
     # pretrained='modelzoo://resnet50',
-    pretrained='/home/g1007540910/RDA_imagenet_GCP/checkpoints/imagenet/old_resnet50/model_best_single.pth.tar',
+    pretrained='/home/g1007540910/checkpoints/imagenet/old_resnet50/model_best_single.pth.tar',
     backbone=dict(
         type='ResNet',
         depth=50,
