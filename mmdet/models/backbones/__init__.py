078fa2d2a9cf3c65e26b42a350f7395817599d9c
13952522076 <xuma@my.unt.edu>
2020-08-31 10:18:43 -0500

    update

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,10 +2,10 @@ from .resnet import ResNet
 # from .resnext import ResNeXt
 # from .ssd_vgg import SSDVGG
 # from .resnet_sge import ResNetSGE
-# from .resnet_se import ResNetSE
+from .resnet_se import ResNetSE
 # from .resnet_cbam import ResNetCBAM
 # from .resnet_bam import ResNetBAM
-# from .resnet_gc import ResNetGC
+from .resnet_gc import ResNetGC
 # from .resnet_sk import ResNetSK
 # from .resnet_se_con6 import ResNetSEC
 # from .resnet_gc_res1 import ResNetGCDCA
@@ -16,4 +16,4 @@ from .resnet import ResNet
 # from .resnet_new3 import ResNetNew3
 from .resnet_spa import ResNetSPA
 
-__all__ = ['ResNet','ResNetSPA']
+__all__ = ['ResNet','ResNetSPA','ResNetSE','ResNetGC']
