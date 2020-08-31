673c537fe83f287db20bb8b02c3085b2740b40f9
13952522076 <xuma@my.unt.edu>
2020-08-31 10:50:54 -0500

    update

--- detimage.py
+++ detimage.py
@@ -3,12 +3,12 @@ from mmcv.runner import load_checkpoint
 from mmdet.models import build_detector
 from mmdet.apis import inference_detector, show_result
 
-cfg = mmcv.Config.fromfile('local_configs/cascade_rcnn_r50_fpn_20e_pretrain_spa_resnet50.py')
+cfg = mmcv.Config.fromfile('local_configs/retinanet_r50_fpn_2x_pretrain_spa_resnet50.py')
 cfg.model.pretrained = None
 
 # construct the model and load checkpoint
 model = build_detector(cfg.model, test_cfg=cfg.test_cfg)
-_ = load_checkpoint(model, './work_dirs/cascade_sparesnet50/epoch_24.pth')
+_ = load_checkpoint(model, './work_dirs/retinanet_r50_fpn_2x_pretrain_spa_resnet50/epoch_24.pth')
 
 # test a single image
 # img = mmcv.imread('test.jpg')
@@ -23,5 +23,5 @@ for i, result in enumerate(inference_detector(model, imgs, cfg, device='cuda:0')
     # visualize the results in a new window
     # show_result(img, result, model.CLASSES)
     # or save the visualization results to image files
-    outname='det_cascade_sparesnet50_'+imgs[i]
+    outname='det_retina_sparesnet50_'+imgs[i]
     show_result(imgs[i], result, out_file=outname)
